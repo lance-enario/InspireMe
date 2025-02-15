83c20d72639cd68bd76256eb5f8175c9dcca4435
Lance <lancejoseph.enario@gmail.com>
2025-02-16 00:10:24 +0800

    initial commit

---
+++ app/src/main/java/com/application/inspireme/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.application.inspireme
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.register_screen)
+    }
+}
