c5344c566878437eb2cab0921078a284968ddff7
Lance <lancejoseph.enario@gmail.com>
2025-02-18 20:26:12 +0800

    pfp page and home page somewhat created.

--- app/src/main/java/com/application/inspireme/LoginActivity.kt
+++ app/src/main/java/com/application/inspireme/LoginActivity.kt
@@ -1,6 +1,5 @@
 package com.application.inspireme
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
@@ -10,16 +9,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : Activity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.profile_page)
-
-//        val button = findViewById<TextView>(R.id.signin)
-//        button.setOnClickListener {
-//            val intent = Intent(this,LoginActivity::class.java)
-//            startActivity(intent)
-//        }
+        enableEdgeToEdge()
+        setContentView(R.layout.login)
+        val button = findViewById<Button>(R.id.btnreg)
+        button.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
