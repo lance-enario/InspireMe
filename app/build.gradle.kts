d9d08b89387a5af91f4cc1a2b8fbe55591f22d23
Lance <lancejoseph.enario@gmail.com>
2025-02-18 18:16:44 +0800

    adjustments for working program

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.application.inspireme"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.application.inspireme"
