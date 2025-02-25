fc413588be2fdf17561e3c155f7d5adb6791c962
Lance <lancejoseph.enario@gmail.com>
2025-02-25 23:40:42 +0800

    wew

--- app/src/main/java/com/application/inspireme/LoginActivity.kt
+++ app/src/main/java/com/application/inspireme/LoginActivity.kt
@@ -3,32 +3,34 @@ package com.application.inspireme
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import android.util.Patterns
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class LoginActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+//        val emaill = "test@gmail.com"
+//        val password = "123"
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
 
+        val buttonLogin = findViewById<Button>(R.id.LoginButton)
+        buttonLogin.setOnClickListener {
+            val usernameInput = findViewById<EditText>(R.id.Username).text.toString().trim()
+            val passwordInput = findViewById<EditText>(R.id.Password).text.toString().trim()
 
-            val button_login = findViewById<Button>(R.id.LoginButton)
-            button_login.setOnClickListener {
-                val username = (findViewById<EditText>(R.id.Username)).text.toString().trim()
-                val password = (findViewById<EditText>(R.id.Password)).text.toString().trim()
+            if (!Patterns.EMAIL_ADDRESS.matcher(usernameInput).matches()) {
+                Toast.makeText(this, "Invalid email format!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
 
-                if(username != "test" || password != "123"){
-                    Toast.makeText(this, "Invalid Credentials. Please try again!", Toast.LENGTH_SHORT).show()
-                } else {
+//            if (usernameInput != emaill || passwordInput != password) {
+//                Toast.makeText(this, "Invalid Credentials. Please try again!", Toast.LENGTH_SHORT).show()
+//            } else {
                 val intent = Intent(this, LandingPageActivity::class.java)
                 startActivity(intent)
-            }
+//            }
         }
     }
 }
