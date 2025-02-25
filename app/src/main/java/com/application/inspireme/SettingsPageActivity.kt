0fe605008739deeaec37df0399c91b5fffc5f80a
Lance <lancejoseph.enario@gmail.com>
2025-02-25 23:40:42 +0800

    settings screen connection to about page, logout function, login ui change

--- app/src/main/java/com/application/inspireme/SettingsPageActivity.kt
+++ app/src/main/java/com/application/inspireme/SettingsPageActivity.kt
@@ -1,20 +1,51 @@
 package com.application.inspireme
 
+import android.app.Activity
+import android.app.ActivityOptions
+import android.app.Dialog
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.constraintlayout.widget.ConstraintLayout
 
-class SettingsPageActivity : AppCompatActivity() {
+class SettingsPageActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_settings_page)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(R.layout.settings_screen)
+
+        val aboutButton = findViewById<ConstraintLayout>(R.id.about_constraint)
+        aboutButton.setOnClickListener {
+            val aboutintent = Intent(this, AboutActivity::class.java)
+            startActivity(aboutintent)
         }
+
+        val backButton = findViewById<ImageView>(R.id.back_icon_left)
+        backButton.setOnClickListener {
+            val backintent = Intent(this, LandingPageActivity::class.java)
+            startActivity(backintent)
+        }
+    }
+    fun showLogoutDialog(view: View) {
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.dialog_logout)
+        dialog.setCancelable(false)
+
+        val cancelButton = dialog.findViewById<Button>(R.id.cancel_button)
+        val logoutButton = dialog.findViewById<Button>(R.id.logout_button)
+
+        cancelButton.setOnClickListener { dialog.dismiss() }
+        logoutButton.setOnClickListener {
+            Toast.makeText(this, "Logged out", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            dialog.dismiss()
+        }
+
+        dialog.show()
     }
 }
